bdda10cdc810563e9537526fd040947cbbe7d5dd
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2021-10-09 00:15:05 -0300

    corrige desafio07

--- js-ninja-desafios/desafios-aulas/desafio07.js
+++ js-ninja-desafios/desafios-aulas/desafio07.js
@@ -19,8 +19,7 @@ function addItem(item) {
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-arr.push(['Leandro', 41, false]);
-console.log(arr[5]);
+console.log(addItem(['Leandro', 41, false]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
