6c7dcf8eea5975c591efb7396e52d64f8876c05f
Raphael Righetti <raphaelrighettiramos@hotmail.com>
2021-10-28 09:01:32 -0300

    resolvendo desafio25

--- js-ninja-desafios/desafios-aulas/desafio25/desafio25.js
+++ js-ninja-desafios/desafios-aulas/desafio25/desafio25.js
@@ -1,19 +1,28 @@
-/*
-Essa semana você terá dois desafios:
-1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
-tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
-ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
-o que não ficou tão claro das aulas anteriores.
-É essencial que você entenda todo o conteúdo que foi passado até aqui,
-para que possamos prosseguir para a parte mais avançada do curso :D
-2) Estudar eventos!
-Acesse a página do MDN:
-https://developer.mozilla.org/en-US/docs/Web/Events#Categories
-Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
-desafio os experimentos legais que você conseguir desenvolver :D
-*/
+(function(window, document) {
+    /*
+    Essa semana você terá dois desafios:
+    1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
+    tudo o que foi passado! Se tiver dúvidas, anote, e então abra issues,
+    ou comente no seu pull request mesmo, que eu irei ajudá-lo a entender
+    o que não ficou tão claro das aulas anteriores.
+    É essencial que você entenda todo o conteúdo que foi passado até aqui,
+    para que possamos prosseguir para a parte mais avançada do curso :D
+    2) Estudar eventos!
+    Acesse a página do MDN:
+    https://developer.mozilla.org/en-US/docs/Web/Events#Categories
+    Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
+    desafio os experimentos legais que você conseguir desenvolver :D
+    */
 
-/* 
-fazer um input que formata a data 
-automaticamente enquanto o usuário escreve
-*/
+    var $date = document.querySelector('[data-js="data"]');
+
+    $date.addEventListener('input', formatDate, false);
+
+    function formatDate(event) {
+        var regexPutSlash = /(\d{2})(\d+)/;
+        var regexRemoveNotNumber = /[^\d/]+/g;
+        if($date.value.length <= 6)
+            $date.value = $date.value.replace(regexPutSlash, '$1/$2');
+        $date.value = $date.value.replace(regexRemoveNotNumber, '');
+    }
+})(window, document);
